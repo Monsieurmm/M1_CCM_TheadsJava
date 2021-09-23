05bb7d6956a70f78e553a277ba605fa84f68218c
Monsieurmm <51748513+Monsieurmm@users.noreply.github.com>
2021-09-23 14:35:58 +0200

    fixup! thread demo sample

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -29,7 +29,7 @@ public class DemoApplication {
 				new Thread(new TestThread(socket,agenda)).start();
 			}
 
-		}catch (IOException e){
+		} catch (IOException e){
 			e.printStackTrace();
 		}
 	}
