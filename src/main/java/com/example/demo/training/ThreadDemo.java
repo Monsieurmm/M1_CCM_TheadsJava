04b4cf7993752942d438b7e01f5bbecd22c46ca1
Monsieurmm <51748513+Monsieurmm@users.noreply.github.com>
2021-09-23 14:26:17 +0200

    thread demo sample

---
+++ src/main/java/com/example/demo/training/ThreadDemo.java
@@ -0,0 +1,43 @@
+package com.example.demo.training;
+
+public class ThreadDemo implements Runnable {
+    private Thread t;
+    private String threadName;
+
+    ThreadDemo( String name) {
+        threadName = name;
+        System.out.println("Creating " +  threadName );
+    }
+
+    public void run() {
+        System.out.println("Running " +  threadName );
+        try {
+            for(int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+                Thread.sleep(50);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " +  threadName + " interrupted.");
+        }
+        System.out.println("Thread " +  threadName + " exiting.");
+    }
+
+    public void start () {
+        System.out.println("Starting " +  threadName );
+        if (t == null) {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+}
+
+class Test {
+
+    public static void main(String args[]) {
+        ThreadDemo R1 = new ThreadDemo( "Thread-1");
+        R1.start();
+
+        ThreadDemo R2 = new ThreadDemo( "Thread-2");
+        R2.start();
+    }
+}
