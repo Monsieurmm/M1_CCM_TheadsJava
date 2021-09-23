ab9d39b16f08a6080a5a2e5fc7fa6026d5841d28
= <=>
2021-09-23 12:03:28 +0200

    Partie Nicolas Davenne

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.JavaBeans.Entity.Agenda;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+        agenda.printAll();
+    }
+}
